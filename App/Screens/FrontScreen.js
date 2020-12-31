00ae26682c805f43f99d1abb159b6403a15cb0b2
udaraWelikndage <udara.welikandage@dimolanka.com>
2020-12-31 20:25:31 +0530

    login button

--- App/Screens/FrontScreen.js
+++ App/Screens/FrontScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Alert, Button, Image, ImageBackground , StyleSheet , Text} from 'react-native';
+import { Alert, Button, Image, ImageBackground , StyleSheet , Text ,View} from 'react-native';
 
 function FrontScreen() {
     return (
        <ImageBackground source={require("../Assets/visa.jpg")} style={styles.background}>
-           <Button title="Sign Up" color="green" onPress={()=>Alert.alert('clicked')}/>
-           <Button title="login" style={styles.loginButton} onPress={()=>Alert.alert('clicked')}/>
+           <View style={styles.view}>
+            <Text style={styles.leftButton} onPress={()=>Alert.alert('clicked')}>Sign Up</Text>
+            <Text style={styles.RightButton} onPress={()=>Alert.alert('clicked')}>Login</Text>
+           </View>
+           {/* <Button title="Sign Up" color="green" onPress={()=>Alert.alert('clicked')}/>
+           <Button title="login" style={styles.loginButton} onPress={()=>Alert.alert('clicked')}/> */}
        </ImageBackground>
     );
 }
@@ -21,8 +25,31 @@ const styles = StyleSheet.create({
         alignSelf:'center',
         bottom:25,
     },
-    loginButton:{
-        color:'white'
+    leftButton:{
+        width:150,
+        padding:10,
+        fontSize:18,
+        textAlign:'center',
+        backgroundColor:'green',
+        height: 50,
+        color:'white',
+        bottom:10,
+        marginLeft:20
+    },
+    RightButton:{
+        width:150,
+        padding:10,
+        fontSize:18,
+        textAlign:'center',
+        backgroundColor:'#2a6fdb',
+        height: 50,
+        color:'white',
+        bottom:10,
+        marginRight:20
+    },
+    view:{
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     }
 })
 
